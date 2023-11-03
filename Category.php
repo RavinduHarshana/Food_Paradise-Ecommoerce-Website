03e4b467b8bb750d9ebe2e5886195229d7f4d048
RavinduHarshana <ravinduharshana2001@gmail.com>
2023-11-04 00:45:20 +0530

    create footer

---
+++ Category.php
@@ -0,0 +1,24 @@
+<!-- Header -->
+<?php
+
+?>
+<html>
+<!-- Search -->
+
+<!-- Category -->
+
+
+
+<head>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./styles/bootstrap.css">
+</head>
+
+<body class="container">
+
+    
+</body>
+
+</html>
+
+<!-- footer -->

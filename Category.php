e9763af32b20b259e7fde877c7719cd03650d485
RavinduHarshana <ravinduharshana2001@gmail.com>
2023-11-04 01:15:48 +0530

    Card Design

--- Category.php
+++ Category.php
@@ -7,18 +7,50 @@
 
 <!-- Category -->
 
-
-
-<head>
-    <link rel="stylesheet" href="style.css">
+<body>
     <link rel="stylesheet" href="./styles/bootstrap.css">
-</head>
-
-<body class="container">
-
     
+    <div class="row gap-4 text-center container">
+
+        <div class="card" style="width: 18rem;">
+            <img src="images\Category\Category-pizza.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's
+                    content.</p>
+              
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="images\Category\Category-burger.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's
+                    content.</p>
+          
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="images\Category\Category-roas.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's
+                    content.</p>
+              
+            </div>
+        </div>
+
+    </div>
+
+
 </body>
 
 </html>
 
 <!-- footer -->
+<?php
+// include "footer.php";
+?>

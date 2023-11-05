088deb23b2fe71bed24fc248aa83ea3bbde26040
RavinduHarshana <ravinduharshana2001@gmail.com>
2023-11-05 15:01:53 +0530

    add Out of Stock message And Condtion check

--- Category_item.php
+++ Category_item.php
@@ -17,12 +17,17 @@ include "dbcon.php";
         <?php
         $catsql = "SELECT * FROM fooditem WHERE Category='$id';";
         $cattbl = mysqli_query($conn, $catsql);
+        if(mysqli_num_rows($cattbl)>0){
+            
+        }
+        else{
+            echo "<h1>$id Is Out of Stock</h1>";
+        }
         while ($item = mysqli_fetch_array($cattbl)) {
-        
         ?>
 
         <div id="clickableDiv" onclick="locate('<?php  echo $item[0];?>')" class="card mycard" style="  width:18rem; ">
-            <h4 class="card-title"><?php echo $item[1]?></h4>
+            <h4 class="card-title mt-3 fa"><?php echo $item[1]?></h4>
             <div class="imgd">
 
                 <img src="images/Food_items/<?php echo $item[2]?>" class="card-img-top" alt="...">
@@ -35,7 +40,7 @@ include "dbcon.php";
             <button type="button" class="btn m-0 p-2 btn-primary">Oder</button>
         </div>
         <?php
-         };
+         }; 
       ?>
 
     </div>

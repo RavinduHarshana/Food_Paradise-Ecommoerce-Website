f4390b4a450e4f48900947a4a61d1f15d59fd579
RavinduHarshana <ravinduharshana2001@gmail.com>
2023-11-05 14:37:09 +0530

    Create Category Item page Dynamicly genarated

--- Category_item.php
+++ Category_item.php
@@ -3,17 +3,45 @@ include "dbcon.php";
 ?>
 
 <body>
-    <h1>Welcome to Another Page</h1>
+<link rel="stylesheet" href="./styles/category.css">
+
     
     <?php
     // Retrieve the id parameter from the URL
     $id = isset($_GET['id']) ? $_GET['id'] : null;
 
-    if ($id !== null) {
-        echo "<p>You passed the ID parameter: $id</p>";
-    } else {
-        echo "<p>No ID parameter passed.</p>";
-    }
-    ?>
+?>
+
+<div class="row cat gap-4 text-center container-fluid ">
+
+        <?php
+        $catsql = "SELECT * FROM fooditem WHERE Category='$id';";
+        $cattbl = mysqli_query($conn, $catsql);
+        while ($item = mysqli_fetch_array($cattbl)) {
+        
+        ?>
+
+        <div id="clickableDiv" onclick="locate('<?php  echo $item[0];?>')" class="card mycard" style="  width:18rem; ">
+            <h4 class="card-title"><?php echo $item[1]?></h4>
+            <div class="imgd">
+
+                <img src="images/Food_items/<?php echo $item[2]?>" class="card-img-top" alt="...">
+            </div>
+            <div class="card-body">
+                <p class="card-text"><?php echo $item[3]?></p>
+
+            </div>    
+            
+            <button type="button" class="btn m-0 p-2 btn-primary">Oder</button>
+        </div>
+        <?php
+         };
+      ?>
+
+    </div>
 </body>
 </html>
+
+<?php 
+    include_once "footer.php";
+?>
